ef294c737039a2cced08c582447e2c1c215b19c3
Jack Sawyer <84543199+JackSawyerWATX@users.noreply.github.com>
2022-09-01 11:13:16 -0700

    first commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    enum size {
+        tall, grande, venti
+    }
+
+    public static void main(String[] args) {
+        size myBev = size.venti;
+        System.out.println(myBev);
+    }
+}
